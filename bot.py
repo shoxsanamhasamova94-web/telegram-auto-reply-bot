cec9ef0190bb238308868c03c8339e732144a4c5
shoxsanamhasamova94-web <shoxsanamhasamova94@gmail.com>
2026-04-23 17:01:55 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,29 @@ import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, CommandHandler, ContextTypes, filters
 
-TOKEN = os.getenv("8624570442:AAFeOR9XT0UyI0d20KWNw_Uyy9-q9LYnqXk")
+import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, MessageHandler, CommandHandler, ContextTypes, filters
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+if not TOKEN:
+    raise ValueError("BOT_TOKEN topilmadi! Railway Variables ni tekshir")
+
+AUTO_REPLY = "Assalomu alaykum, hozir men offline man."
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Salom! Bot ishlayapti.")
+
+async def auto_reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(AUTO_REPLY)
+
+app = ApplicationBuilder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, auto_reply))
+
+app.run_polling()
 
 if not TOKEN:
     raise ValueError("BOT_TOKEN topilmadi! Railway Variables ni tekshir")
